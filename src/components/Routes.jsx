e0ee8a59a76bf1282918f286bfd6b62823a08832
ZoeBarrios <zbarrios@frsn.utn.edu.ar>
2023-11-02 21:20:29 -0300

    Creando otros directorios

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,8 +1,6 @@
 import { Route } from "wouter";
 import Home from "../pages/Home";
 import Auth from "../pages/Auth";
-import Publication from "../pages/Publication";
-import Profile from "../pages/Profile";
 
 function Routes() {
   return (
