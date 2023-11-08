757bb7dd982146948dc07bcd9089ddd7f1284fba
Diego <ezequielgomezdie@gmail.com>
2023-11-07 21:03:15 -0300

    agregando ofertas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,6 +30,52 @@ function Home() {
 
       <Categorias></Categorias>
 
+
+<div className="fondo_ofertas">
+
+<div className="cont_ofertas">
+
+<div className="card_ofertas" id="oferta1">
+<div className="des_oferta">
+<h3>Weekend Discount</h3>
+<h2>Game Consoles</h2>
+<p>Best Choice of The Year...</p>
+<p>from <span className="negro">$249.99</span></p>
+</div>
+</div>
+<div className="card_ofertas" id="oferta2">
+<div className="des_oferta">
+<h3>Weekend Discount</h3>
+<h2>Home Speaker</h2>
+<p>Your music.Make it vibrant</p>
+<p>from <span className="negro">$559.99</span></p>
+</div>
+</div>
+<div className="card_ofertas" id="oferta3">
+<div className="des_oferta">
+<h3>Weekend Discount</h3>
+<h2>Accessories</h2>
+<p>Only for this week...</p>
+<p>from <span className="negro">$129.99</span></p>
+</div>
+</div>
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
       <div className="fondo2">
         <div className="titulo_fondo2">
           <h1>PRODUCTS</h1>
