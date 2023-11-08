d8af7f45b13e16ff8d539f0f0f2db18602ad34a0
ZoeBarrios <zbarrios@frsn.utn.edu.ar>
2023-11-08 16:41:08 -0300

    Agregando loader

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import useForm from "../hooks/useForm";
 import { login } from "../services/auth";
 import AuthContext from "../context/authContext";
 import { isEmailOrUsername } from "../services/users";
 import { useLocation } from "wouter";
 import { toast } from "react-toastify";
+import { useMutation } from "react-query";
+import Loader from "./Loader";
 
 export const Login = (props) => {
   const { handleLogin } = useContext(AuthContext);
   const [location, setLocation] = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
   const { formData, handleInputChange, resetForm } = useForm({
     email: "",
     password: "",
   });
   const { email, password } = formData;
+  const { mutate } = useMutation(login, {
+    onMutate: () => {
+      setIsLoading(true);
+    },
+    onSuccess: (user) => {
+      handleLogin(user);
+      setLocation("/");
+    },
+    onError: (error) => {
+      toast(error.message, { autoClose: 1500, type: "error" });
+    },
+    onSettled: () => {
+      setIsLoading(false);
+    },
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -37,48 +55,50 @@ export const Login = (props) => {
       loginData.userName = email;
     }
 
-    login({ ...loginData, password })
-      .then((user) => {
-        handleLogin(user);
-        setLocation("/");
-      })
-      .catch((error) => {
-        toast(error.message, { autoClose: 1500, type: "error" });
-      });
+    mutate({
+      ...loginData,
+      password,
+    });
   };
 
   return (
     <div className="auth-form-container login">
-      <h2 className="title">Log in</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label id="email-id" htmlFor="email">
-          Email or Username{" "}
-        </label>
-        <input
-          value={email}
-          type="text"
-          placeholder="email o username"
-          id="email"
-          name="email"
-          onChange={handleInputChange}
-        />
-        <label htmlFor="password">Password </label>
-        <input
-          value={password}
-          type="password"
-          placeholder="******"
-          id="password"
-          name="password"
-          onChange={handleInputChange}
-        />
-        <button type="submit">Log In</button>
-      </form>
-      <button
-        className="link-btn"
-        onClick={() => props.onFormSwitch("register")}
-      >
-        Don't have an account? Register clicking here.
-      </button>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <h2 className="title">Log in</h2>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <label id="email-id" htmlFor="email">
+              Email or Username{" "}
+            </label>
+            <input
+              value={email}
+              type="text"
+              placeholder="email o username"
+              id="email"
+              name="email"
+              onChange={handleInputChange}
+            />
+            <label htmlFor="password">Password </label>
+            <input
+              value={password}
+              type="password"
+              placeholder="******"
+              id="password"
+              name="password"
+              onChange={handleInputChange}
+            />
+            <button type="submit">Log In</button>
+          </form>
+          <button
+            className="link-btn"
+            onClick={() => props.onFormSwitch("register")}
+          >
+            Don't have an account? Register clicking here.
+          </button>
+        </>
+      )}
     </div>
   );
 };
