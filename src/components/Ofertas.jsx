18217e3579bc9332ae9269f1ec9c539a809665b1
Diego <ezequielgomezdie@gmail.com>
2023-11-07 23:06:22 -0300

    Agregando otra seccion

---
+++ src/components/Ofertas.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Ofertas() {
+  return (
+    <div>
+        <div className="fondo_ofertas">
+
+<div className="cont_ofertas">
+
+<div className="card_ofertas" id="oferta1">
+<div className="des_oferta">
+<h3>Weekend Discount</h3>
+<h2>Game Consoles</h2>
+<p>Best Choice of The Year...</p>
+<p>from <span className="negro">$249.99</span></p>
+</div>
+</div>
+<div className="card_ofertas" id="oferta2">
+<div className="des_oferta">
+<h3>Weekend Discount</h3>
+<h2>Home Speaker</h2>
+<p>Your music.Make it vibrant</p>
+<p>from <span className="negro">$559.99</span></p>
+</div>
+</div>
+<div className="card_ofertas" id="oferta3">
+<div className="des_oferta">
+<h3>Weekend Discount</h3>
+<h2>Accessories</h2>
+<p>Only for this week...</p>
+<p>from <span className="negro">$129.99</span></p>
+</div>
+</div>
+
+
+
+</div>
+
+
+</div>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,24 @@
 import Header from "../components/Header";
 import Categorias from "../components/Categorias";
 import Footer from "../components/Footer";
-import ListOfPublications from "../components/ListOfPublications";
+import Icono1 from "../imgs/icono1.png";
+import Icono2 from "../imgs/icono2.png";
+import Icono3 from "../imgs/icono3.png";
+import Icono4 from "../imgs/icono4.png";
 
+import ListOfPublications from "../components/ListOfPublications";
+import Ofertas from "../components/Ofertas";
+import Servicios from "../components/Servicios";
 function Home() {
+
+
+
   return (
     <div>
       <Header></Header>
 
       <div className="fondo1">
-        <div className="cont_fondo1">
+        <div data-aos="fade-down" className="cont_fondo1">
           <h4>
             In <span className="letra_verde">MarketHub</span>
           </h4>
@@ -21,51 +30,32 @@ function Home() {
             We have prepared special discounts for you on electronic <br />
             products. Dont miss these opportunities...
           </p>
+
+          <a className="shop" href="#prod">Shop Now <i class="fa-solid fa-arrow-right"></i></a>
         </div>
       </div>
+
+<Servicios></Servicios>
+
+<Ofertas></Ofertas>
+
+
+
+
+
+
+
+
 
       <div className="titulo_cate">
         <h1>Categorys</h1>
       </div>
 
+
+
       <Categorias></Categorias>
 
 
-<div className="fondo_ofertas">
-
-<div className="cont_ofertas">
-
-<div className="card_ofertas" id="oferta1">
-<div className="des_oferta">
-<h3>Weekend Discount</h3>
-<h2>Game Consoles</h2>
-<p>Best Choice of The Year...</p>
-<p>from <span className="negro">$249.99</span></p>
-</div>
-</div>
-<div className="card_ofertas" id="oferta2">
-<div className="des_oferta">
-<h3>Weekend Discount</h3>
-<h2>Home Speaker</h2>
-<p>Your music.Make it vibrant</p>
-<p>from <span className="negro">$559.99</span></p>
-</div>
-</div>
-<div className="card_ofertas" id="oferta3">
-<div className="des_oferta">
-<h3>Weekend Discount</h3>
-<h2>Accessories</h2>
-<p>Only for this week...</p>
-<p>from <span className="negro">$129.99</span></p>
-</div>
-</div>
-
-
-
-</div>
-
-
-</div>
 
 
 
